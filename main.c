0cf7bb1b622ad15a8c0504199f9bc8674a6e3c63
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-09 15:25:52 +0900

    Q2

--- main.c
+++ main.c
@@ -5,11 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-	char code1 = 'A';
-	char code2 = 65;
+	int i = 0;
+	char str[4];
+	str[0] = 'a';
+	str[1] = 'b';
+	str[2] = 'c';
+	str[3] = '\0';
 	
-	printf("code1 = %c, (%d)\n", code1, code1);
-	printf("code2 = %c, (%d)\n", code2, code2);
+	printf("%s\n", str);
 	
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);//문자열이 맨끝에 도달하지 않을때까지 반복한다. 
+		i++;
+	}
+	system("PAUSE");
 	return 0;
 }
