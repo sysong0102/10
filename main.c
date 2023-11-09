993179627b9c5a6651e15c440458a09aa2f82241
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-09 15:49:29 +0900

    Q5

--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 
 int main(int argc, char *argv[])
 {
-	char src[30] = "happy C programming"; //30글자는 아님  
+	FILE* fp; //fp는 변수 이름 
+	char str[100];
+	int i;
+	//1. open file
+	fp = fopen("sample.txt", "w"); //출력할때는 쓰기모드 
 	
-	printf("문자열 \"%s\" 의 길이: %i\n ", src, strlen(src));
 	
+	//2. write file
+	for (i = 0; i<3; i++)
+	{
 	
+	//2-1. print " input a word "
+	printf("input a word : "); 
+	//2-2. scanf a string 
+	scanf("%s", str); //이 자체로 포인터기 때문에 &넣을 필요 없음 
+	//2-3. fprintf()
+	fprintf(fp, "%s\n", str); 
+	}
+	
+	//3. close file
+	fclose(fp);
 	system("PAUSE");
 	return 0;
 }
